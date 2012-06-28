a578b4b926fff881ca86e63a357f8b933a84b2a1
Fred Bricon <fbricon@gmail.com>
2012-06-28 18:23:33 +0200

    383355 : Convert existing EJB project to Maven
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

---
+++ org.eclipse.m2e.wtp.tests/src/org/eclipse/m2e/wtp/tests/conversion/EjbProjectConversionTest.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *      Red Hat, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.m2e.wtp.tests.conversion;
+
+/**
+ * Test EJB project conversion
+ *  
+ * @author Fred Bricon
+ */
+public class EjbProjectConversionTest extends AbstractWtpProjectConversionTestCase {
+
+	@Override
+	protected String getPackagingUnderTest() {
+		return "ejb";
+	}
+
+
+	public void testEjb31ProjectConversion() throws Exception {
+		testProjectConversion("ejb31");
+	}
+	
+	public void testNoCustomizationNeededProjectConversion() throws Exception {
+		// Checks an EJB 2.1 project with maven layout and Java 1.5 produces
+		// a minimal pom.xml
+		testProjectConversion("ejb21");
+	}	
+}
