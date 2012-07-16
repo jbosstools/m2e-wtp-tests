f3bb2b255b86cf50409cc9d165056382fbe02fb0
Fred Bricon <fbricon@gmail.com>
2012-07-16 20:05:19 +0200

    383357 : Convert existing application client project to Maven
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

---
+++ org.eclipse.m2e.wtp.tests/src/org/eclipse/m2e/wtp/tests/conversion/AppClientProjectConversionTest.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *      Red Hat, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.m2e.wtp.tests.conversion;
+
+/**
+ * Test Application-client project conversion
+ *  
+ * @author Fred Bricon
+ */
+public class AppClientProjectConversionTest extends AbstractWtpProjectConversionTestCase {
+
+	@Override
+	protected String getPackagingUnderTest() {
+		return "app-client";
+	}
+
+
+	public void testAppClientProjectConversion() throws Exception {
+		testProjectConversion("app-client-1");
+	}
+
+	public void testMinimalAppClientProjectConversion() throws Exception {
+		testProjectConversion("app-client-2");
+	}
+	
+
+}
