265c796c2a4b61b7e47983171c2e74981c783f2f
Fred Bricon <fbricon@gmail.com>
2013-05-06 21:38:18 +0200

    407315 : refactored for Facet Detection API
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- org.eclipse.m2e.wtp.tests/src/org/eclipse/m2e/wtp/tests/facets/FacetDetectorManagerTest.java
+++ org.eclipse.m2e.wtp.tests/src/org/eclipse/m2e/wtp/tests/facets/FacetDetectorManagerTest.java
@@ -1,7 +1,19 @@
+/*************************************************************************************
+ * Copyright (c) 2011-2013 Red Hat, Inc. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Fred Bricon (Red Hat, Inc.) - initial API and implementation
+ ************************************************************************************/
 package org.eclipse.m2e.wtp.tests.facets;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jst.j2ee.web.project.facet.WebFacetUtils;
+import org.eclipse.m2e.core.MavenPlugin;
+import org.eclipse.m2e.core.project.IMavenProjectRegistry;
 import org.eclipse.m2e.core.project.ResolverConfiguration;
 import org.eclipse.m2e.wtp.facets.FacetDetectorManager;
 import org.eclipse.m2e.wtp.tests.AbstractWTPTestCase;
@@ -14,8 +26,14 @@ public class FacetDetectorManagerTest extends AbstractWTPTestCase {
 		FacetDetectorManager facetDetectorManager = FacetDetectorManager.getInstance();
 		//TestFacetDetector2 satisfies both detectors. First one by priority 
 		//(org.eclipse.m2e.wtp.tests.facets.TestFacetDetector2) must win
-		assertEquals(WebFacetUtils.WEB_22, facetDetectorManager.findFacetVersion(projects[0], null, "foo.bar", monitor));
+		IMavenProjectRegistry registry = MavenPlugin.getMavenProjectRegistry();
+		
+		assertEquals(WebFacetUtils.WEB_22, facetDetectorManager.findFacetVersion(registry.getProject(projects[0]), 
+				                           "foo.bar", 
+				                           monitor));
 		//maven only satisfies the 2nd detector
-		assertEquals(WebFacetUtils.WEB_30, facetDetectorManager.findFacetVersion(projects[1], null, "foo.bar", monitor));
+		assertEquals(WebFacetUtils.WEB_30, facetDetectorManager.findFacetVersion(registry.getProject(projects[1]), 
+				                           "foo.bar", 
+				                           monitor));
 	}
 }
--- org.eclipse.m2e.wtp.tests/src/org/eclipse/m2e/wtp/tests/facets/TestFacetDetector2.java
+++ org.eclipse.m2e.wtp.tests/src/org/eclipse/m2e/wtp/tests/facets/TestFacetDetector2.java
@@ -1,21 +1,32 @@
+/*************************************************************************************
+ * Copyright (c) 2011-2013 Red Hat, Inc. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Fred Bricon (Red Hat, Inc.) - initial API and implementation
+ ************************************************************************************/
 package org.eclipse.m2e.wtp.tests.facets;
 
 import java.util.Map;
 
-import org.apache.maven.project.MavenProject;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jst.j2ee.web.project.facet.WebFacetUtils;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.wtp.facets.AbstractFacetDetector;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
 
 public class TestFacetDetector2 extends AbstractFacetDetector {
 
 	@Override
-	public IProjectFacetVersion findFacetVersion(IProject project,
-			MavenProject mavenProject, Map<?, ?> context,
-			IProgressMonitor monitor) throws CoreException {
+	public IProjectFacetVersion findFacetVersion(IMavenProjectFacade mavenProjectFacade, Map<?, ?> context, IProgressMonitor monitor) {
+		IProject project = mavenProjectFacade.getProject();
+		if (project == null) {
+			return null;
+		}
 		if ("detect-my-name".equals(project.getName())) {
 			return WebFacetUtils.WEB_22;
 		}
